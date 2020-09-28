99b9a8039e329f38e5f936b398170f9e17442d09
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2020-09-28 14:43:15 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
 // JavaScript Document
 
+var form = document.getElementById("form")
+var email = document.getElementById("email")
+var input = document.querySelector("input")
+var succesElement = document.getElementById("succesmessage")
 
+form.addEventListener('submit', function (event) {
+  if (input.value) {
+    succesElement.innerText = input.value + ' heeft zich succesvol ingeschreven!'
+  } else {
+    succesElement.innerText = 'Oeps er ging iets fout met het invoeren van uw e-mail'
+  }
+  
+  event.preventDefault()
+})
