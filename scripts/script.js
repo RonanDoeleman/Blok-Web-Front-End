9ae9191e34ac8e09750e87420e2d9018fdaa1b7b
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2020-10-06 01:26:34 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,47 @@ form.addEventListener('submit', function (event) {
   
   event.preventDefault()
 })
+
+
+
+
+
+// -- HAMBURGER MENI --
+//de menu knop
+var menubutton = document.querySelector(".menu-button");
+
+//de hamburger menu
+var hamburgermenu = document.querySelector(".hamburger-menu");
+
+//de sluitknop van de hamburger menu
+var menusluiten = document.querySelector(".hamburger-menu-sluiten"); 
+
+
+//hier wordt er geklikt op de menu knop
+menubutton.addEventListener("click", menuOpenen);
+
+//hier wordt er geklikt op de sluit knop
+menusluiten.addEventListener("click", menuSluiten);
+
+
+
+function menuOpenen() {
+    
+    //als de hamburgermenu op display none staat wordt het een block als er geklikt wordt op de knop
+    if(hamburgermenu.style.display === "none") {
+        hamburgermenu.style.display = "block";
+        
+    //als de hamburgermenu geopend is en er wordt weer geklikt op de knop dan komt er een display none
+    } else {
+        hamburgermenu.style.display = "none";
+    }
+
+}
+
+
+
+function menuSluiten(){
+    
+    //de hamburgermenu krijgt display none
+    hamburgermenu.style.display = "none";
+}
